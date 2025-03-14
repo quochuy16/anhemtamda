f4582d748f0e0426d85957e968e4e9efa5b9e428
quochuy16 <103881090+quochuy16@users.noreply.github.com>
2025-03-14 13:16:32 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'], // Hỗ trợ trình duyệt cũ
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'] // Hỗ trợ async/await
+    })
+  ],
   esbuild: {
     jsx: "automatic",
   },
